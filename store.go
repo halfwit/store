6395f880b18f53a55f3b31c424899e9cbe3ebc92
halfwit <michaelmisch1985@gmail.com>
2019-01-23 20:31:12 -0700

    Honor the same flags as plumb (-i can be added if it's requested)

--- store.go
+++ store.go
@@ -1,13 +1,23 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"mime"
 	"net/http"
 	"net/url"
 	"os"
+	"strings"
 
 	"9fans.net/go/plumb"
+)
+
+var (
+	plumbfile = flag.String("p", "/mnt/plumb/send", "write the message to plumbfile (default /mnt/plumb/send)")
+	attributes = flag.String("a", "", "set the attr field of the message (default is empty), expects key=value")
+	source = flag.String("s", "", "set the src field of the message (default is store)")
+	destination = flag.String("d", "", "set the dst filed of the message (default is empty)")
+	directory = flag.String("w", "", "set the wdir field of the message (default is current directory)")
 )
 
 type storeMsg struct {
@@ -36,16 +46,26 @@ func (s storeMsg) send() error {
 	return message.Send(fd)
 }
 
-func newStoreMsg(mediaType, wdir string, attr *plumb.Attribute) *storeMsg {
-	return &storeMsg{
+func newStoreMsg(mediaType, wdir, arg string, attr *plumb.Attribute) *storeMsg {
+	sf := &storeMsg{
 		src: os.Args[0],
 		dst: "",
 		wdir: wdir,
 		msgtype: mediaType,
 		attr: attr,
-		ndata: len(os.Args[1]),
-		data: os.Args[1],
+		ndata: len(arg),
+		data: arg,
 	}
+	if *plumbfile != "" {
+		sf.src = *plumbfile
+	}
+	if *source != "" {
+		sf.src = *source
+	}
+	if *destination != "" {
+		sf.dst = *destination
+	}
+	return sf
 }
 
 func paramsToAttr(params map[string]string) *plumb.Attribute {
@@ -54,6 +74,10 @@ func paramsToAttr(params map[string]string) *plumb.Attribute {
 	for key, value := range params {
 		attr.Name = key
 		attr.Value = value
+	}
+	if *attributes != "" {
+		attr.Name = strings.TrimLeft(*attributes, "=")
+		attr.Value = strings.TrimRight(*attributes, "=")
 	}
 	return attr
 }
@@ -80,26 +104,29 @@ func content(testUrl string) (string, error) {
 }
 
 func main() {
-	if (len(os.Args) != 2) {
-		log.SetFlags(0)
-		log.Fatalf("Usage: %s <URL>\n", os.Args[0])
+	flag.Parse()
+	if flag.Lookup("h") != nil {
+		flag.Usage()
+		os.Exit(1)
 	}
 	wdir, err := os.Getwd()
 	if err != nil {
 		log.Fatal(err)
 	}
-	ct, err := content(os.Args[1])
-	if err != nil {
-		log.Fatalf("Error fetching content-type for URL: %s\n", err)
-	}
-	mediaType, params, err := mime.ParseMediaType(ct)
-	if err != nil {
-		log.Fatal(err)
-	}
-	attr := paramsToAttr(params)
-	storeMsg := newStoreMsg(mediaType, wdir, attr)
-	err = storeMsg.send()
-	if err != nil {
-		log.Fatal(err)
+	for _, arg := range os.Args[1:] {
+		ct, err := content(arg)
+		if err != nil {
+			log.Fatalf("Error fetching content-type for URL: %s\n", err)
+		}
+		mediaType, params, err := mime.ParseMediaType(ct)
+		if err != nil {
+			log.Fatal(err)
+		}
+		attr := paramsToAttr(params)
+		storeMsg := newStoreMsg(mediaType, wdir, arg, attr)
+		err = storeMsg.send()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 }
